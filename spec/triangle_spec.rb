136c43d19fb7f6a65dd22c16ebeb9d3238637455
nirasan <niranirao@gmail.com>
2015-04-14 17:16:23 +0900

    実装と正常系のテスト

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
-  specify {
-    expect(Triangle.name_for(0,0,0)).to eq ""
-  }
+  it "name_for method" do
+    expect(Triangle.name_for(1, 1, 1)).to eq "正三角形ですね！"
+    expect(Triangle.name_for(2, 2, 1)).to eq "二等辺三角形ですね！"
+    expect(Triangle.name_for(2, 3, 4)).to eq "不等辺三角形ですね！"
+    expect(Triangle.name_for(1, 2, 3)).to eq "三角形じゃないです＞＜"
+  end
+
+  it "triangle? method" do
+    expect(Triangle.triangle?(1, 2, 3)).to eq false
+    expect(Triangle.triangle?(2, 3, 4)).to eq true
+    expect(Triangle.triangle?(2, 2, 1)).to eq true
+    expect(Triangle.triangle?(1, 1, 1)).to eq true
+  end
 end
--- triangle.rb
+++ triangle.rb
@@ -1,5 +1,27 @@
 class Triangle
   def self.name_for(n1, n2, n3)
-    ""
+    if self.triangle?(n1, n2, n3)
+      if self.equilateral_triangle?(n1, n2, n3)
+        "正三角形ですね！"
+      elsif self.isosceles_triangle?(n1, n2, n3)
+        "二等辺三角形ですね！"
+      else
+        "不等辺三角形ですね！"
+      end
+    else
+      "三角形じゃないです＞＜"
+    end
+  end
+
+  def self.triangle?(n1, n2, n3)
+    n1 + n2 > n3 && n2 + n3 > n1 && n3 + n1 > n2
+  end
+
+  def self.equilateral_triangle?(n1, n2, n3)
+    self.triangle?(n1, n2, n3) && n1 == n2 && n2 == n3 && n3 == n1
+  end
+
+  def self.isosceles_triangle?(n1, n2, n3)
+    self.triangle?(n1, n2, n3) && !self.equilateral_triangle?(n1, n2, n3) && (n1 == n2 || n2 == n3 || n3 == n1)
   end
 end
